42afc99a328bfa9e3452df24e10c46f95f1c0e42
GaboCordero <gabocordero2005@gmail.com>
2026-06-25 05:51:52 -0600

    Commit Semana 2

---
+++ InitializrSpringbootProject/src/main/java/com/tiendaTech/tienda/TiendaTechApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaTech.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaTechApplication.class, args);
+	}
+
+}
---
+++ InitializrSpringbootProject/src/test/java/com/tiendaTech/tienda/TiendaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaTech.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
